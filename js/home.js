7315d3cdf24edf1435fe36d57ee97ea491e3e104
neel2902 <nilkamalthakuria2902@gmail.com>
2020-04-04 17:23:28 +0530

    functions ++

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+let HOST = "https://bits-sports.org/sports/";
 let input, filter, table, tr, td, i, txtValue;
 table = document.getElementById("table");
 tr = table.getElementsByTagName("tr");
@@ -27,3 +28,41 @@ const search = (index) => {
         }
     }
 }
+// Search ends here
+////////////////////////////////////////////////////////////////////////
+const person = (serialno, name, aadhar, sector, amount, application_link) => {
+    const table_row = `
+    <tr>
+    <th scope="row">${serialno}</th>
+    <td>${name}</td>
+    <td>${aadhar}</td>
+    <td>${sector}</td>
+    <td class="text-center">${amount}/-</td>
+    <td><a href="application.html">${application_link}</a></td>
+    </tr>
+    `
+    return table_row;
+}
+
+const populate = () => {
+    tbody = $("#tbody");
+    let person_i = person(1, 2, 3, 4, 5, 6);
+    tbody.append(person_i);
+    tbody.append(person_i);
+    tbody.append(person_i);
+}
+
+populate();
+
+var myHeaders = new Headers();
+myHeaders.append("X-AUTH", "0ac6859b-8fa9-4d7a-b24e-61e4a42b6079");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+};
+
+fetch("https://finhelp.herokuapp.com/register/get-documents/", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));

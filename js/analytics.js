68aef001b43f2da9f86689590da36fe5ec09f3c5
neel2902 <nilkamalthakuria2902@gmail.com>
2020-04-04 15:30:32 +0530

    analytics added

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+        [5, 2, 4, 2, 0]
+    ]
+};
+
+new Chartist.Line('.ct-chart', data);
